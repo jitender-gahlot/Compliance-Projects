044c052cab1efa2154833aff960de59750f55e38
Jitender Gahlot <mail2gahlot@gmail.com>
2019-02-01 21:54:28 +0300

    Adding Kerberos Configs

--- src/main/java/sa/com/alrajhi/spring/webservice/alrajhiwebservice/controller/FileUploadController.java
+++ src/main/java/sa/com/alrajhi/spring/webservice/alrajhiwebservice/controller/FileUploadController.java
@@ -1,12 +1,16 @@
 package sa.com.alrajhi.spring.webservice.alrajhiwebservice.controller;
 
+import org.apache.hadoop.conf.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.hadoop.security.UserGroupInformation;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,24 +20,43 @@ public class FileUploadController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.ALL_VALUE)
     public String handleMapping(@RequestParam("fileupload") MultipartFile file) {
         String FileName = file.getName();
-        String x1=null;
+        String x1 = null;
         List list = new ArrayList<String>();
         try {
             byte[] bytes = file.getBytes();
             String membersId = new String(bytes);
             String[] id = membersId.split(",");
 
-            for (String x:id)
-            {
+            for (String x : id) {
                 System.out.println(x);
                 list.add(x);
 
             }
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         System.out.println(FileName);
+
+        try {
+
+            Configuration conf = new Configuration();
+            conf.set("hadoop.security.authentication", "Kerberos");
+
+            UserGroupInformation.setConfiguration(conf);
+            UserGroupInformation.loginUserFromKeytab("jeet/quickstart.cloudera@CLOUDERA","D:\\Development\\GIT\\Compliance-Projects\\alrajhi-webservice\\src\\main\\resources\\security\\jeet.keytab");
+
+            Class.forName("com.cloudera.impala.jdbc41.Driver");
+            Connection connection = null;
+            //Class.forName(DRIVER_CLASS);
+            String CONNECTION_URL = "jdbc:impala://quickstart.cloudera:21050;AuthMech=1;KrbRealm=CLOUDERA;KrbHostFQDN=quickstart.cloudera;KrbServiceName=impala";
+            connection = DriverManager.getConnection(CONNECTION_URL);
+            System.out.println("Connection From IMPALA: " + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         return list.toString();
     }
 }
