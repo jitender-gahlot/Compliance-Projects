87a61b3fd2460fa4e2f3b6aa8b1f5572ede4f4db
Jitender Gahlot <mail2gahlot@gmail.com>
2019-02-01 14:01:08 +0300

    new change

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -1,79 +1,38 @@
-$(function () {
+function readURL(input) {
+  if (input.files && input.files[0]) {
 
-    var totalTime = 6, // seconds
-        percent = 0,
-        $sIcon = $('.success-icon'),
-        $sText = $('.success-text'),
-        $info = $('.timer-info'),
-        $bar = $('.timer-bar'),
-        $dropZone = $('.drop-zone'),
-        dropZone = $('.drop-zone')[0],
-        width = $dropZone.width(),
-        countdown,
-        countdownOver = false;
+    var reader = new FileReader();
 
-    var startCountdown = function () {
-        $dropZone.css('border', 'none');
-        $dropZone.addClass('timer-bar-wrapper');
-        $info.text('0%');
-        countdown = setInterval(updateBar, 25);
+    reader.onload = function(e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
     };
 
-    $(window).resize(function () {
-        if (countdownOver) {
-            width = $dropZone.width();
-            $dropZone.css('height', width);
-        }
+    reader.readAsDataURL(input.files[0]);
+
+  } else {
+    removeUpload();
+  }
+}
+
+function removeUpload() {
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  $('.file-upload-content').hide();
+  $('.image-upload-wrap').show();
+}
+
+function submitUpload() {
+
+     $('#uploadform').attr('action', '/upload');
+  };
+
+$('.image-upload-wrap').bind('dragover', function () {
+        $('.image-upload-wrap').addClass('image-dropping');
     });
-
-    var fileStatus = function () {
-        $sIcon
-            .addClass('up')
-            .delay(250)
-            .fadeTo(250, 1, 'swing');
-        $sText
-            .text('File uploaded')
-            .delay(500)
-            .fadeTo(350, 1, 'swing');
-        $dropZone.css('transition', 'none');
-    };
-
-    var triggerFinish = function () {
-        $dropZone
-            .css('height', width)
-            .addClass('expand');
-        $info.fadeTo(250, 0, 'swing');
-        setTimeout(fileStatus, 700);
-    };
-
-    var stopCountdown = function () {
-        clearInterval(countdown);
-        $('.timer-info').text('100%');
-        triggerFinish();
-    };
-
-    var updateBar = function () {
-        percent++;
-        // /40, because it's called every 25ms
-        // -> 25ms * 40 = 1 sec
-        var per = (100 * percent / totalTime) / 40;
-        $bar.css('width', per + '%');
-        $info
-            .css('left', per + '%')
-            .text(per.toFixed(1) + '%');
-        if (per >= 100) {
-            stopCountdown();
-            countdownOver = true;
-        }
-    };
-
-    dropZone.addEventListener('dragover', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-    });
-    dropZone.addEventListener('drop', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        setTimeout(startCountdown, 250)
-    });
+    $('.image-upload-wrap').bind('dragleave', function () {
+        $('.image-upload-wrap').removeClass('image-dropping');
 });
