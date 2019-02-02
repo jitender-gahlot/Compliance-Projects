c203e72b55b101a3b88857bb6190f5da0af0dc41
Jitender Gahlot <mail2gahlot@gmail.com>
2019-02-02 10:45:41 +0300

    Kerberos Config complete

--- src/main/java/sa/com/alrajhi/spring/webservice/alrajhiwebservice/controller/FileUploadController.java
+++ src/main/java/sa/com/alrajhi/spring/webservice/alrajhiwebservice/controller/FileUploadController.java
@@ -11,13 +11,15 @@ import org.apache.hadoop.security.UserGroupInformation;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class FileUploadController {
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.ALL_VALUE)
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public String handleMapping(@RequestParam("fileupload") MultipartFile file) {
         String FileName = file.getName();
         String x1 = null;
@@ -41,25 +43,34 @@ public class FileUploadController {
         try {
 
             Configuration conf = new Configuration();
-            conf.set("hadoop.security.authentication", "Kerberos");
+            System.setProperty("sun.security.krb5.debug", "true");
+            System.setProperty("java.security.krb5.conf", "D:/Development/GIT/Compliance-Projects/alrajhi-webservice/src/main/resources/security/krb5.conf");
+            System.setProperty("java.security.auth.login.config","D:/Development/GIT/Compliance-Projects/alrajhi-webservice/src/main/resources/security/jaas.conf");
+            System.setProperty("java.security.auth.login.config","D:/Development/GIT/Compliance-Projects/alrajhi-webservice/src/main/resources/security/jaas.conf");
+            System.setProperty("java.security.krb5.conf", "D:/Development/GIT/Compliance-Projects/alrajhi-webservice/src/main/resources/security/krb5.conf");
+            System.setProperty("hadoop.security.authentication", "Kerberos");
             //conf.set("javax.security.auth.useSubjectCredsOnly", "true");
-            conf.set("java.security.krb5.conf", "D:\\Development\\GIT\\Compliance-Projects\\alrajhi-webservice\\src\\main\\resources\\security\\krb5.conf");
-            conf.set("java.security.auth.login.config","D:\\Development\\GIT\\Compliance-Projects\\alrajhi-webservice\\src\\main\\resources\\security\\jaas.conf");
+            System.out.println("Here");
 
+            System.out.println("Here1");
+
+            System.out.println("Here2");
             UserGroupInformation.setConfiguration(conf);
-            UserGroupInformation.loginUserFromKeytab("jeet/quickstart.cloudera@CLOUDERA","D:\\Development\\GIT\\Compliance-Projects\\alrajhi-webservice\\src\\main\\resources\\security\\jeet.keytab");
+            System.out.println("Here3");
 
 //            UserGroupInformation.loginUserFromKeytab("jeet/quickstart.cloudera@CLOUDERA","/Development/GIT/Compliance-Projects/src/main/resources/security/jeet.keytab");
-
-
-
-            
 
             Class.forName("com.cloudera.impala.jdbc41.Driver");
             Connection connection = null;
             //Class.forName(DRIVER_CLASS);
-            String CONNECTION_URL = "jdbc:impala://quickstart.cloudera:21050;AuthMech=1;KrbRealm=CLOUDERA;KrbHostFQDN=quickstart.cloudera;KrbServiceName=impala";
+            String CONNECTION_URL = "jdbc:impala://quickstart.cloudera:21050;AuthMech=1;KrbHostFQDN=quickstart.cloudera;KrbServiceName=impala";
             connection = DriverManager.getConnection(CONNECTION_URL);
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("Select * from customers");
+            if(results.next()){
+
+                System.out.println(results.getString("id"));
+            }
             System.out.println("Connection From IMPALA: " + connection);
         } catch (Exception e) {
             e.printStackTrace();
